f773426c29bfaef1ee6af8c33758df86c3f9bee9
Ramsey Merdassi <merdassi.r@gmail.com>
2024-09-13 09:44:18 +0100

    fixed warning and added styling

--- rps.php
+++ rps.php
@@ -1,40 +1,77 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <style>
+
+        form {
+            display: flex;
+            justify-content: center;
+        }
+
+        input {
+            padding: 2rem;
+            margin: 0.4rem;
+        }
+
+        h4 {
+            display: flex;
+            justify-content: center;
+        }
+
+        h2 {
+            display: flex;
+            justify-content: center;
+        }
+
+        .rock {
+            background-color: #df20b6;
+        }
+
+        .scissors {
+            background-color: #1ecbe1;
+        }
+
+        .paper {
+            background-color: #e1d81e;
+        }
+
+    </style>
+</head>
 <body>
 <form action="rps.php" method="post">
-    <input type="submit" value="rock" name="guess">
-    <input type="submit" value="paper" name="guess">
-    <input type="submit" value="scissors" name="guess">
+    <input class='rock' type="submit" value="rock" name="guess">
+    <input class='paper' type="submit" value="paper" name="guess">
+    <input class='scissors' type="submit" value="scissors" name="guess">
 </form>
 </body>
 </html>
 
 <?php
 
-if (isset($_POST['guess'])) {
-    $userGuess = $_POST['guess'];
-    echo "<br>You choose ".$userGuess;
-}
-
-
+$userGuess = $_POST['guess'] ?? null;
 $options = ['rock', 'paper', 'scissors'];
 
-$computerGuess = $options[rand(0, 1)];
 
-if ($userGuess === $computerGuess)
-{
-    echo "<br>Computer chooses $computerGuess<br>"
-    ."It's a draw";
-}
-elseif (($userGuess === $options[0] && $computerGuess === $options[1]) ||
+if (isset($_POST['guess'])) {
+    echo "<br><h4>You choose ".$userGuess.'</h4>';
+    $computerGuess = $options[rand(0, 1)];
+
+    if ($userGuess === $computerGuess)
+    {
+        echo "<br><h4>Computer chooses $computerGuess.</h4><br>"
+            ."<h2>It's a draw</h2>";
+    }
+    elseif (($userGuess === $options[0] && $computerGuess === $options[1]) ||
         ($userGuess === $options[1] && $computerGuess === $options[2]) ||
         ($userGuess === $options[2] && $computerGuess === $options[0]))
-{
-    echo "<br>Computer chooses $computerGuess<br>"
-    ."Computer wins";
+    {
+        echo "<br><h4>Computer chooses $computerGuess</h4><br>"
+            ."<h2>Computer wins</h2>";
+    }
+    else
+    {
+        echo "<br><h4>Computer chooses $computerGuess</h4><br>"
+            ."<h2>You win</h2>";
+    }
 }
-else
-{
-    echo "<br>Computer chooses $computerGuess<br>"
-    ."You win";
-}
+
